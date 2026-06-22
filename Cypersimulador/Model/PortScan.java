fc10dc5c52a3562e4bc7f95762dfdeff41479c7c
Danilo Santos <danilosantos.contato27@gmail.com>
2026-06-22 19:25:46 -0300

    Add files via upload

---
+++ Cypersimulador/Model/PortScan.java
@@ -0,0 +1,35 @@
+package model;
+
+//================================= Declaracao da classe herdeira ==========================
+public class PortScan extends Incidente {
+    
+    private static int idPS = 0;
+    private int portasVarridas;
+
+    public PortScan(String ipOrigem, String ipDestino) {
+        super("PS-" + (++idPS), ipOrigem, ipDestino);
+        this.portasVarridas = 0;
+    }
+
+//============================ Consulta e incrementação do atributo ===========================
+    public int getPortasVarridas(){ 
+        return portasVarridas; 
+    }
+
+    public void incrementarPortas(){
+        this.portasVarridas++;
+    }
+
+//================================= Implementação das intefaces ==============================
+    @Override
+    public void executarMitigacao(){
+        System.out.println("  >> [PortScan] Ativando firewall para o IP: " + getIpOrigem()
+            + " | Portas varridas: " + portasVarridas);
+    }
+
+    @Override
+    public String gerarLinhaAuditoria(){
+        return String.format("[%s] ID: %s | TIPO: Port Scan | ORIGEM: %s | DESTINO: %s | NUM.PORTAS: %d",
+            getTimestampFormatado(), getId(), getIpOrigem(), getIpDestino(), portasVarridas);
+    }
+}
