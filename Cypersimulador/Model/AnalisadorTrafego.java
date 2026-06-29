ad3d3e554dcfa02357dcddee7079aa257876d7f1
smmy <samuelaugusto075@gmail.com>
2026-06-29 10:56:28 -0300

    implementação do analisadortrafego levando em consideração as threads

--- Cypersimulador/Model/AnalisadorTrafego.java
+++ Cypersimulador/Model/AnalisadorTrafego.java
@@ -1,9 +1,27 @@
-public abstract class AnalisadorTrafego {
-    protected SOCEngine engine;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-    public AnalisadorTrafego(SOCEngine engine){
+public abstract class AnalisadorTrafego implements Runnablegit  {
+    protected SOCEngine engine;
+    protected String caminhoArquivo;
+
+    public AnalisadorTrafego(SOCEngine engine, String caminhoArquivo) {
         this.engine = engine;
+        this.caminhoArquivo = caminhoArquivo;
     }
 
     public abstract void processarLinhaBruta(String linhaLog);
+
+    @Override
+    public void run() {
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                processarLinhaBruta(linha);
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o log na thread " + Thread.currentThread().getName() + ": " + e.getMessage());
+        }
+    }
 }
