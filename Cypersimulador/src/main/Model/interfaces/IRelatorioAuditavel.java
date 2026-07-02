14939c42ad84135d1d4c0ace58b7ce69bbdfde74
smmy <samuelaugusto075@gmail.com>
2026-07-02 14:40:46 -0300

    adicionando javadoc na interface relatorioAuditavel

--- Cypersimulador/src/main/Model/interfaces/IRelatorioAuditavel.java
+++ Cypersimulador/src/main/Model/interfaces/IRelatorioAuditavel.java
@@ -1,5 +1,14 @@
-package model.interfaces;
+package Model.interfaces;
 
-public interface IRelatorioAuditavel{
+/**
+ * Interface para garantir que todo incidente gere uma linha de auditoria.
+ * Muito útil para compliance e rastreabilidade.
+ */
+public interface IRelatorioAuditavel {
+    /**
+     * Gera uma linha formatada para o relatório de auditoria.
+     * 
+     * @return String contendo os dados do incidente
+     */
     String gerarLinhaAuditoria();
 }
