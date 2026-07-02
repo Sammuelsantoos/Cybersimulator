00538e4bd9c1e2521a14051a6838c93d924c63f9
smmy <samuelaugusto075@gmail.com>
2026-07-02 14:51:07 -0300

    adicionando teste (deve criar forcabruta com ips validos) IncidentTest

--- Cypersimulador/src/test/Model/entities/IncidenteTest.java
+++ Cypersimulador/src/test/Model/entities/IncidenteTest.java
@@ -5,3 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import Model.exceptions.IpInvalidoException;
+
+/**
+ * Testes unitários para validação de incidentes e IPs.
+ */
+public class IncidenteTest {
+
+    @Test
+    public void deveCriarForcaBrutaComIPsValidos() throws IpInvalidoException {
+        ForcaBruta fb = new ForcaBruta("192.168.0.1", "10.0.0.5");
+        assertNotNull(fb.getId());
+        assertEquals("192.168.0.1", fb.getIpOrigem());
+        assertEquals("10.0.0.5", fb.getIpDestino());
+        assertEquals(5, fb.getTentativasFalhas());
+    }
